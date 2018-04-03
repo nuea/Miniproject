24d62224bb83f8b1848df827d803d90d2402fa96
Nuea <nueha.pimkanok@gmail.com>
2018-04-03 13:43:31 +0700

    update

---
+++ Totoro/assets/step.js
@@ -0,0 +1,60 @@
+var currentTab = 0;
+showTab(currentTab); 
+function showTab(n) {
+    var x = document.getElementsByClassName("tab");
+    x[n].style.display = "block";
+    if (n == 0) {
+        document.getElementById("prevBtn").style.display = "none";            
+        document.getElementById("fsubmit").style.display = "none";
+    }
+    else if (n == (x.length - 1)) {
+        document.getElementById("fsubmit").style.display= "inline";            
+        document.getElementById("nextBtn").style.display = 'none';            
+        document.getElementById("prevBtn").style.display = "none";
+    } 
+    else {
+        document.getElementById("prevBtn").style.display = "inline";      
+    }
+
+    fixStepIndicator(n)
+}
+
+function nextPrev(n) {
+    var x = document.getElementsByClassName("tab");
+    if (n == 1 && !validateForm()) return false;
+    x[currentTab].style.display = "none";
+    currentTab = currentTab + n;
+    if (currentTab >= x.length) {
+        document.getElementById("regForm").submit();
+        return false;
+    }
+    showTab(currentTab);
+}
+
+function validateForm() {
+    var x, y, i, valid = true;
+    x = document.getElementsByClassName("tab");
+    y = x[currentTab].getElementsByTagName("input");
+    // A loop that checks every input field in the current tab:
+    for (i = 0; i < y.length; i++) {
+        // If a field is empty...
+        if (y[i].value == "") {
+        // add an "invalid" class to the field:
+        y[i].className += " invalid";
+        // and set the current valid status to false
+        valid = false;
+        }
+    }
+    if (valid) {
+        document.getElementsByClassName("step")[currentTab].className += " finish";
+    }
+    return valid; 
+}
+
+function fixStepIndicator(n) {
+    var i, x = document.getElementsByClassName("step");
+    for (i = 0; i < x.length; i++) {
+        x[i].className = x[i].className.replace(" active", "");
+    }
+    x[n].className += " active";
+}
